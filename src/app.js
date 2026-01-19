cdeac00028839666e571dc2b5f2d05f03c13e323
sanket chalwadi <sanket.s.chalwadi@gmail.com>
2026-01-19 11:07:14 +0530

    adding backend on git

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use("/api", pokemonRoutes);
+app.use("/api/pokemon", pokemonRoutes);
 
 app.get("/", (req, res) => {
   res.send("Pokédex API running");
